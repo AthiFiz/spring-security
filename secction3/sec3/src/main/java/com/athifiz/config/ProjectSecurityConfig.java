9a1e6e34c9dee6deb2de31d8db5ff6b21266b1a0
AthiFiz <athif.rasheedh@gmail.com>
2025-12-18 23:07:10 +0530

    in memory authorization

---
+++ secction3/sec3/src/main/java/com/athifiz/config/ProjectSecurityConfig.java
@@ -0,0 +1,95 @@
+package com.athifiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+//        http.authorizeHttpRequests((request) -> request.anyRequest().permitAll());
+//        http.authorizeHttpRequests((request) -> request.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/account", "/balance", "/loans", "/cards").authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.formLogin(withDefaults());
+//        http.formLogin(formLoginConfigurer -> formLoginConfigurer.disable());
+        http.httpBasic(withDefaults());
+//        http.httpBasic(httpBasicConfigurer -> httpBasicConfigurer.disable());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+//        UserDetails user = User.withUsername("user").password("{noop}123").authorities("read").build();
+        UserDetails user = User.withUsername("user").password(passwordEncoder().encode("123")).authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("$2a$12$d6bxlxdDnHArHfC03pvhdOpnsS4b44qtBNqcykadTuDFIkhW.Kio.").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // by default this will BCryptPasswordEncoder
+    }
+
+
+
+//    @Bean
+//    public CompromisedPasswordChecker compromisedPasswordChecker(){
+//        return new HaveIBeenPwnedRestApiPasswordChecker();
+//    }
+
+
+
+
+
+
+
+//    --------------------------------------------------------------------------------
+
+//        Better Approach
+
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails user = User.withUsername("user")
+//    //            by default, it will use {bcrypt}
+//                .password(passwordEncoder().encode("123"))
+//                .authorities("read")
+//                .build();
+//
+//    //    if we are using the passwordEncoder like this, have to mention the id - {bcrypt}
+//        UserDetails admin = User.withUsername("admin")
+//                .password("{bcrypt}$2a$12$d6bxlxdDnHArHfC03pvhdOpnsS4b44qtBNqcykadTuDFIkhW.Kio.")
+//                .authorities("admin")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // by default this will BCryptPasswordEncoder
+//    }
+
+//    --------------------------------------------------------------------------------
+
+
+}
---
+++ secction3/sec3/src/test/java/com/athifiz/sec1/FizBankAppTests.java
@@ -0,0 +1,13 @@
+package com.athifiz.sec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizBankAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
